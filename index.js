5956498cf1fa8572abbed58740218b430f710a35
Dillon Porter <dillonporter@hotmail.com>
2023-11-07 17:27:27 -0330

    updated JS bug on the images

--- index.js
+++ index.js
@@ -1,45 +1,31 @@
-const IMAGE_COUNT = 4;
-const PRODUCT_PRICE = 125.00;
+// Initialize the amountValue at the top level of the script
 let amountValue = 0;
-let currentImg = 1;
+let currentIndex = 1;
 
-// Cache DOM elements
+// Select DOM elements
 const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.querySelector(".close-btn");
 const menu = document.querySelector(".nav_links");
 const overlay = document.querySelector(".overlay");
 const mainThumbnail = document.querySelector(".main-thumbnail");
-const mainThumbnailLightBox = document.querySelector(".lightbox-container .main-thumbnail");
-const images = document.querySelectorAll(".preview img");
-const plusBtn = document.querySelector("#plus");
-const minusBtn = document.querySelector("#minus");
-const amount = document.querySelector(".amount");
+const lightbox = document.querySelector(".lightbox");
+const lightboxMainThumbnail = document.querySelector(".lightbox .main-thumbnail");
+const thumbnails = document.querySelectorAll(".preview img");
+const amountDisplay = document.querySelector(".amount");
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("previous");
 const thumbMob = document.querySelector(".thumb-mob");
 const cartBtn = document.querySelector(".cart-btn");
-const cart = document.querySelector(".cart-wrp");
+const cartWrapper = document.querySelector(".cart-wrp");
 const closeLightboxBtn = document.querySelector(".close-lightbox");
-const lightbox = document.querySelector(".lightbox");
-const addBtn = document.querySelector(".add_btn");
-const indicator = document.querySelector(".indicator");
-const wrp = document.querySelector(".cart-content");
+const addToCartButton = document.querySelector(".add_btn");
+const cartIndicator = document.querySelector(".indicator");
+const cartContent = document.querySelector(".cart-content");
 
-// Hide the cart indicator initially
-indicator.style.display = "none";
+// Set the cart indicator to be hidden initially
+cartIndicator.style.display = "none";
 
-// Utility functions
-function updateThumbnailImage(imageNumber) {
-    const imagePath = `./images/image-product-${imageNumber}.jpg`;
-    mainThumbnail.src = imagePath;
-    mainThumbnailLightBox.src = imagePath;
-}
-
-function toggleElementVisibility(element, isVisible) {
-    element.style.display = isVisible ? 'block' : 'none';
-}
-
-// Event handler functions
+// Function definitions
 function openMenu() {
     menu.classList.add("active");
     overlay.classList.add("active");
@@ -52,118 +38,119 @@ function closeMenu() {
 
 function handlePlus() {
     amountValue++;
-    amount.innerText = amountValue;
+    amountDisplay.textContent = amountValue;
 }
 
 function handleMinus() {
     if (amountValue > 0) {
         amountValue--;
-        amount.innerText = amountValue;
+        amountDisplay.textContent = amountValue;
     }
 }
 
 function nextImage() {
-    currentImg = currentImg % IMAGE_COUNT + 1;
-    thumbMob.src = `./images/image-product-${currentImg}.jpg`;
+    currentIndex = currentIndex === 4 ? 1 : currentIndex + 1;
+    updateMainImage(currentIndex, true);
 }
 
 function prevImage() {
-    currentImg = (currentImg + IMAGE_COUNT - 2) % IMAGE_COUNT + 1;
-    thumbMob.src = `./images/image-product-${currentImg}.jpg`;
+    currentIndex = currentIndex === 1 ? 4 : currentIndex - 1;
+    updateMainImage(currentIndex, true);
+}
+
+function updateMainImage(index, isLightbox = false) {
+    const newImageSrc = `./images/image-product-${index}.jpg`;
+    if (!isLightbox) {
+        mainThumbnail.src = newImageSrc;
+    } else {
+        lightboxMainThumbnail.src = newImageSrc;
+    }
+    thumbnails.forEach((image) => {
+        image.addEventListener("click", () => {
+            const index = parseInt(image.getAttribute('data-index'));
+            openLightBox(index);
+        });
+    });
+
 }
 
 function toggleCart() {
-    cart.classList.toggle("invisible");
+    cartWrapper.classList.toggle("invisible");
+    overlay.classList.toggle("invisible");
 }
 
 function closeLightBox() {
     lightbox.classList.add("invisible");
+    overlay.classList.add("invisible");
 }
 
-function openLightBox() {
+function openLightBox(index) {
+    currentIndex = index || 1;
     lightbox.classList.remove("invisible");
+    overlay.classList.remove("invisible");
+    updateMainImage(currentIndex, true);
 }
 
 function addItem() {
-    if (amountValue > 0) {
-        const total = PRODUCT_PRICE * amountValue;
-        wrp.classList.remove("empty");
-        wrp.innerHTML = `
+    const total = (125.00 * amountValue).toFixed(2);
+    cartContent.classList.remove("empty");
+    cartContent.innerHTML = `
         <div class="product">
-          <div>
-            <img src="./images/image-product-1-thumbnail.jpg" class="product-img" alt="product">
-            <div class="product-info">
-              <p class="product-title">Fall Limited Edition Sneakers</p>
-              <p>
-                <span>$${PRODUCT_PRICE.toFixed(2)}</span> × 
-                <span class="number">${amountValue}</span>
-                <span> = </span>
-                <b>$${total.toFixed(2)}</b>
-              </p>
+            <div class="product-details">
+                <img src="./images/image-product-1-thumbnail.jpg" class="product-img" alt="product">
+                <div class="product-info">
+                    <p class="product-title">Fall Limited Edition Sneakers</p>
+                    <p><span>$125.00</span> × <span class="number">${amountValue}</span> = <b>$${total}</b></p>
+                </div>
+                <button class="delete-btn">
+                    <img src="./images/icon-delete.svg" alt="delete">
+                </button>
             </div>
-            <button class="delete-btn"><img src="./images/icon-delete.svg" alt="delete"></button>
-          </div>
-          <button class="checkout-btn">Checkout</button>
+            <button class="checkout-btn">Checkout</button>
         </div>`;
-        toggleElementVisibility(indicator, true);
-        indicator.innerText = amountValue;
-    }
+    cartIndicator.style.display = "block";
+    cartIndicator.textContent = amountValue;
+    document.querySelector(".delete-btn").addEventListener("click", deleteItem);
 }
 
-
-
 function deleteItem() {
-    wrp.classList.add("empty");
-    wrp.innerHTML = `<p>Your cart is empty</p>`;
-    toggleElementVisibility(indicator, false);
+    amountValue = 0;
+    amountDisplay.textContent = amountValue;
+    cartContent.classList.add("empty");
+    cartContent.innerHTML = '<p>Your cart is empty</p>';
+    cartIndicator.style.display = "none";
+    cartWrapper.classList.add('invisible');
 }
 
 // Event listeners
-document.querySelector('.preview').addEventListener('click', (e) => {
-    if (e.target.tagName === 'IMG') {
-        images.forEach(img => img.classList.remove('selected'));
-        e.target.classList.add('selected');
-        let selectedImageIndex = Array.from(images).indexOf(e.target) + 1;
-        updateThumbnailImage(selectedImageIndex);
-    }
-});
-
-// Utility function to check if the click is outside of an element
-function clickOutsideElement(event, element) {
-    return !element.contains(event.target);
-}
-
-document.addEventListener('click', function(event) {
-    // Check if the click is outside the cart and the cart is visible
-    if (clickOutsideElement(event, cart) && !cart.classList.contains("invisible")) {
-        // Check if the click is not on the cart button or any of its children
-        if (!event.target.closest('.cart-btn')) {
-            cart.classList.add("invisible");
-        }
-    }
-
-    // Check if the delete button inside the cart was clicked
-    if (event.target.matches('.delete-btn') || event.target.closest('.delete-btn')) {
-        deleteItem();
-    }
-});
-
-
 menuBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", closeMenu);
-plusBtn.addEventListener("click", handlePlus);
-minusBtn.addEventListener("click", handleMinus);
+document.getElementById('plus').addEventListener("click", handlePlus);
+document.getElementById('minus').addEventListener("click", handleMinus);
 nextBtn.addEventListener("click", nextImage);
 prevBtn.addEventListener("click", prevImage);
 cartBtn.addEventListener("click", toggleCart);
 closeLightboxBtn.addEventListener("click", closeLightBox);
-mainThumbnail.addEventListener("click", openLightBox);
-addBtn.addEventListener("click", addItem);
-document.addEventListener('click', function(e) {
-    if (e.target.matches('.delete-btn')) {
-        deleteItem();
+mainThumbnail.addEventListener("click", () => openLightBox());
+addToCartButton.addEventListener("click", () => {
+    addItem();
+    toggleCart(); // Open cart when item is added
+});
+
+thumbnails.forEach((image, index) => {
+    image.addEventListener("click", () => {
+        openLightBox(index + 1); // The index starts from 0 hence add 1
+    });
+});
+
+// Close the cart if the user clicks outside of it
+window.addEventListener('click', function(e) {
+    if (!cartWrapper.contains(e.target) && !cartBtn.contains(e.target) && !cartWrapper.classList.contains('invisible')) {
+        toggleCart();
     }
 });
 
-// Initialize the first image
-updateThumbnailImage(currentImg);
+// Prevent the menu overlay from closing the cart when clicked
+overlay.addEventListener('click', function(e) {
+    e.stopPropagation();
+});
